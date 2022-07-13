f590ae9b420a5689aa2528dd174969b95659d748
brunospcastro <brunoscastro@ua.pt>
2022-07-13 16:34:05 +0100

    database

---
+++ src/main/java/com/weatherinloop/frontend/model/Favorites.java
@@ -0,0 +1,35 @@
+package com.weatherinloop.frontend.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "Favorites")
+public class Favorites {
+
+    @Id
+    @Column(name = "favID")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)    
+    private Integer favID;
+
+    @Column(name = "city")
+    private String city;
+
+    @ManyToMany(mappedBy = "favorites", fetch = FetchType.EAGER,cascade = CascadeType.PERSIST)
+    private Set<User> users = new HashSet<>();
+    
+}
